039ad53e885a0c1b071993d122d5dfa054e96365
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2025-07-17 20:09:21 +0530

    Create Versions.kt

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,7 @@
+object Versions {
+    const val COMMONS_LANG = "3.12.0"
+    const val JUNIT = "4.13.2"
+    const val MOCKITO = "4.11.0"
+    const val SLF4J = "2.0.7"
+    const val JACKSON = "2.15.2"
+}
