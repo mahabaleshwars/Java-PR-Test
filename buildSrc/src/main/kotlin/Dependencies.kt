32cd6de8e6ce90d956ca0f35764e21de7545e229
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2025-07-17 20:10:16 +0530

    Create Dependencies.kt

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,8 @@
+object Dependencies {
+    const val COMMONS_LANG = "org.apache.commons:commons-lang3:${Versions.COMMONS_LANG}"
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val MOCKITO = "org.mockito:mockito-core:${Versions.MOCKITO}"
+    const val SLF4J_API = "org.slf4j:slf4j-api:${Versions.SLF4J}"
+    const val JACKSON_CORE = "com.fasterxml.jackson.core:jackson-core:${Versions.JACKSON}"
+    const val JACKSON_DATABIND = "com.fasterxml.jackson.core:jackson-databind:${Versions.JACKSON}"
+}
