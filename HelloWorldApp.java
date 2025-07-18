02c7bdec9e0b9c4f3d9d6a567700cc3f55189c79
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2025-07-18 16:09:37 +0530

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,9 +1,7 @@
-public class HelloWorldApp {
-    public static void main(String[] args) {
+public class HelloWorldApp { 
+    public static void main(String[] args) { 
         System.out.println("Hello from " + System.getProperty("java.vendor") + " " + System.getProperty("java.version"));
-        System.out.println("Java VM: " + System.getProperty("java.vm.name"));
-        System.out.println("Architecture: " + System.getProperty("os.arch"));
-        System.out.println("Data Model: " + System.getProperty("sun.arch.data.model") + "-bit");
-        System.out.println("Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2025-07-18 10:21:28");
-    }
+        System.out.println("Current Date (UTC): 2025-07-18 07:55:55");
+        System.out.println("Current User: mahabaleshwars");
+    } 
 }
